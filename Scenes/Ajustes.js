6dacbf34e944fd87e6e8857d07f0ad9b6cb2248e
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-05-25 12:33:20 -0600

    Añadir confirmacion de la eliminacion del usuario

--- Scenes/Ajustes.js
+++ Scenes/Ajustes.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 import db, { borrarUsuarios } from "../db";
 
 export default function PaginaAjustes() {
   return (
     <View style={styles.container}>
       <Text>Ventana de ajustes</Text>
-      <Button title="Borrar El Usuario" onPress={() => borrarUsuarios()} />
+      <Button
+        title="Borrar El Usuario"
+        onPress={() => {
+          Alert.alert(
+            "Confirmación",
+            "¿Estás seguro de que quieres continuar?",
+            [
+              {
+                text: "Cancelar",
+                onPress: () => console.log("Borrado Cancelado"),
+                style: "cancel",
+              },
+              {
+                text: "Borrar",
+                onPress: () => borrarUsuarios(),
+              },
+            ],
+            { cancelable: false }
+          );
+        }}
+      />
     </View>
   );
 }
