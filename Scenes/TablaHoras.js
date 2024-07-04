5fec58f97ad0cc08a6ae62bac4c70a8513402cb1
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-07-04 13:30:25 -0600

    Implementado front en la escena tablaHoras.js

--- Scenes/TablaHoras.js
+++ Scenes/TablaHoras.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ImageBackground,
+  Scale,
+} from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import db from "../Modulos/db";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -33,42 +40,100 @@ export default function PaginaTablaHoras() {
     }, [])
   );
 
+  const image = require("../assets/fondo.png");
+
   return (
-    <View style={styles.container}>
-      {MostrarHoras ? (
-        <FlatList
-          data={Horas}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              {item.IsBackedInSupabase == 0 ? (
-                <Ionicons name="cloud-offline" size={30} color="black"/>
-              ) : (
-                <Ionicons name="cloud" size={30} color="black"/>
-              )}
-              <Text>Inicio: {item.Inicio}</Text>
-              <Text>Final: {item.Final}</Text>
-              <Text>Total: {item.Total}</Text>
-            </View>
-          )}
-        />
-      ) : (
-        <Text>No hay registros</Text>
-      )}
-    </View>
+    <ImageBackground source={image} style={styles.container}>
+      <View
+        style={{
+          width: "90%",
+          marginTop: "8%",
+          //gap: 4,
+        }}
+      >
+        <Text style={{ fontSize: Scale > 400 ? 24 : 20, fontWeight: "bold" }}>
+          Tabla de registros
+        </Text>
+        <Text
+          style={{ fontSize: Scale > 400 ? 24 : 20, fontWeight: "regular" }}
+        >
+          Horas formato de total HH:MM:SS
+        </Text>
+      </View>
+      <View style={styles.listContainer}>
+        {MostrarHoras ? (
+          <FlatList
+            data={Horas}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                {item.IsBackedInSupabase == 0 ? (
+                  <Ionicons name="cloud-offline" size={30} color="black" />
+                ) : (
+                  <Ionicons name="cloud" size={30} color="white" />
+                )}
+                <Text style={styles.txt}>Inicio: {item.Inicio}</Text>
+                <Text style={styles.txt}>Final: {item.Final}</Text>
+                <Text style={styles.txt}>Total: {item.Total}</Text>
+              </View>
+            )}
+          />
+        ) : (
+          <Text>No hay registros</Text>
+        )}
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  txtMain: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  txt: {
+    color: "white",
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    paddingTop: 50,
+    justifyContent: "center",
+    resizeMode: "cover",
   },
   item: {
+    backgroundColor: "#2272A7",
+    // width: "100%",
+    // height: "20%",
+    borderRadius: 20,
+    // //borderColor: "white",
+    marginBottom: "8%",
+    //gap: "12%",
+    // //De aqui para abajo son las sombras para los distintos sistemas
+    elevation: 15, //Android
+    shadowColor: "#333333", //A partir de aqui ios
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
     padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
+  },
+  listContainer: {
+    backgroundColor: "#ffffff",
+    width: "90%",
+    height: "70%",
+    borderRadius: 20,
+    marginTop: "8%",
+    padding: "4%",
+    //De aqui para abajo son las sombras para los distintos sistemas
+    elevation: 15, //Android
+    shadowColor: "#333333", //A partir de aqui ios
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
   },
 });
