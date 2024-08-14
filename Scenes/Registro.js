b18331fb5d678dfc1db54b1c76ea4bc46e5085f6
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-08-14 16:41:26 -0600

    Fix en registro

--- Scenes/Registro.js
+++ Scenes/Registro.js
@@ -5,12 +5,17 @@ import {
   View,
   TextInput,
   Dimensions,
-  Button,
+  TouchableOpacity,
   Alert,
   ScrollView,
+  ImageBackground,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
-import { AñadeUsuario, obtenerCentros, obtenerDepartamentos } from "../Modulos/OperacionesBD";
+import {
+  AñadeUsuario,
+  obtenerCentros,
+  obtenerDepartamentos,
+} from "../Modulos/OperacionesBD";
 import { GuardarDatosUsuario } from "../Modulos/InfoUsuario";
 import { CommonActions } from "@react-navigation/native";
 
@@ -36,21 +41,27 @@ export default function PaginaRegistro({ navigation }) {
   }, []);
 
   useEffect(() => {
-    if (selectedCentro) {
+    if (selectedCentro && selectedCentro != "Selecciona una opción") {
       const fetchDepartamentos = async () => {
         const data = await obtenerDepartamentos(selectedCentro);
         setDepartamentos(data);
       };
-
       fetchDepartamentos();
+    } else {
+      setDepartamentos([]); // Cambiar null por un array vacío
     }
-  }, [selectedCentro]);
+}, [selectedCentro]);
+
+  const image = require("../assets/Back.png");
 
   return (
-    <View style={styles.background}>
-      <ScrollView style={styles.scrollView}>
-        <Text style={styles.text}>Hola aspirante, registra tu usuario</Text>
-        <Text style={styles.text}>Escribe tu nombre</Text>
+    <ImageBackground source={image} style={styles.background}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Registro</Text>
+      </View>
+
+      <ScrollView style={styles.formContainer}>
+        <Text style={styles.subtitle}>Nombre</Text>
         <TextInput
           style={styles.input}
           onChangeText={(text) => {
@@ -59,7 +70,7 @@ export default function PaginaRegistro({ navigation }) {
           value={Nombre}
           placeholder="Nombre"
         />
-        <Text style={styles.text}>Escribe tu Codigo</Text>
+        <Text style={styles.subtitle}>Código</Text>
         <TextInput
           style={styles.input}
           onChangeText={(text) => {
@@ -69,19 +80,25 @@ export default function PaginaRegistro({ navigation }) {
           value={codigo}
           placeholder="Codigo"
         />
-        <Text style={styles.text}>Selecciona un rol </Text>
+        <Text style={styles.subtitle}>Rol </Text>
         <View style={{ width: 240, height: 150 }}>
           <Picker
             selectedValue={tipoUsuario}
             itemStyle={styles.text}
             onValueChange={(itemValue) => DeftipoUsuario(itemValue)}
           >
-            <Picker.Item label="Selecciona una opción" value="Selecciona una opción" />
-            <Picker.Item label="Prestador de servicio" value="Prestador de servicio" />
+            <Picker.Item
+              label="Selecciona una opción"
+              value="Selecciona una opción"
+            />
+            <Picker.Item
+              label="Prestador de servicio"
+              value="Prestador de servicio"
+            />
             <Picker.Item label="Practicante" value="Practicante" />
           </Picker>
         </View>
-        <Text style={styles.text}>Selecciona un Centro Universitario</Text>
+        <Text style={styles.subtitle}>Centro Universitario</Text>
         <View style={{ width: 240, height: 150 }}>
           <Picker
             selectedValue={selectedCentro}
@@ -91,22 +108,34 @@ export default function PaginaRegistro({ navigation }) {
               setSelectedDepartamento(null); // Reset the Departamento picker
             }}
           >
-            <Picker.Item label="Selecciona una opción" value="Selecciona una opción" />
+            <Picker.Item
+              label="Selecciona una opción"
+              value="Selecciona una opción"
+            />
             {centros.map((centro) => (
-              <Picker.Item key={centro.id} label={centro.Nombre} value={centro.id} />
+              <Picker.Item
+                key={centro.id}
+                label={centro.Nombre}
+                value={centro.id}
+              />
             ))}
           </Picker>
         </View>
-        {selectedCentro && (
+        {selectedCentro && selectedCentro !== "Selecciona una opción" && departamentos.length > 0 && (
           <>
-            <Text style={styles.text}>Selecciona un Departamento</Text>
+            <Text style={styles.subtitle}>Selecciona un Departamento</Text>
             <View style={{ width: 240, height: 150 }}>
               <Picker
                 selectedValue={selectedDepartamento}
                 itemStyle={styles.text}
-                onValueChange={(itemValue) => setSelectedDepartamento(itemValue)}
+                onValueChange={(itemValue) =>
+                  setSelectedDepartamento(itemValue)
+                }
               >
-                <Picker.Item label="Selecciona una opción" value="Selecciona una opción" />
+                <Picker.Item
+                  label="Selecciona una opción"
+                  value="Selecciona una opción"
+                />
                 {departamentos.map((departamento) => (
                   <Picker.Item
                     key={departamento.id}
@@ -118,7 +147,7 @@ export default function PaginaRegistro({ navigation }) {
             </View>
           </>
         )}
-        <Text style={styles.text}>Escribe tu contraseña </Text>
+        <Text style={styles.subtitle}>Contraseña </Text>
         <TextInput
           style={styles.input}
           secureTextEntry={true}
@@ -128,10 +157,16 @@ export default function PaginaRegistro({ navigation }) {
           value={Contraseña}
           placeholder="Contraseña"
         />
-        <Button
-          title="Listo"
+        <TouchableOpacity
+          style={styles.btnRegistro}
           onPress={() => {
-            if (Nombre !== "" && tipoUsuario !== "" && codigo !== "" && selectedCentro !== null && selectedDepartamento !== null) {
+            if (
+              Nombre !== "" &&
+              tipoUsuario !== "" &&
+              codigo !== "" &&
+              selectedCentro !== null &&
+              selectedDepartamento !== null
+            ) {
               if (codigo.length === 9) {
                 AñadeUsuario(
                   Nombre.toUpperCase(),
@@ -153,41 +188,116 @@ export default function PaginaRegistro({ navigation }) {
               Alert.alert("Por favor rellene todos los datos");
             }
           }}
-        />
+        >
+          <Text style={styles.txtBtn}>Registrar</Text>
+        </TouchableOpacity>
       </ScrollView>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: "cover",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  scrollView: {
-    marginTop: 30
-  },
-  input: {
-    height: Scale > 400 ? 60 : 40,
-    borderWidth: 1,
-    backgroundColor: "white",
-    borderRadius: Scale > 400 ? 20 : 15,
-    padding: 10,
-    margin: 10,
-    width: "80%",
-    fontSize: Scale > 400 ? 30 : 15,
-  },
-  text: {
-    fontSize: Scale > 400 ? 50 : 15,
+  title: {
+    fontSize: Scale > 400 ? 24 : 20,
     fontWeight: "bold",
+    //marginRight: 30,
     color: "black",
   },
-  container: {
+  subtitle: {
+    fontSize: Scale > 400 ? 18 : 14,
+    marginLeft: "4%",
+    color: "black",
+  },
+  txtBtn: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  background: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    resizeMode: "cover",
+  },
+  formContainer: {
+    height: "50%",
+    //marginBottom: "20%",
+    width: "84%",
+  },
+  titleContainer: {
+    alignItems: "center",
+    marginBottom: "10%",
+    marginTop: "55%",
+    //backgroundColor: "green",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    backgroundColor: "#C5E0F2",
+    borderRadius: 50,
+    //De aqui para abajo son las sombras para los distintos sistemas
+    elevation: 15, //Android
+    shadowColor: "#333333", //A partir de aqui ios
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  //Terminan las sombras
+  btnContainer: {
+    alignItems: "center",
+
+    backgroundColor: "yellow",
+    width: "84%",
+    height: "10%",
+  },
+  btnIngresar: {
+    backgroundColor: "#2272A7",
+    height: "20%",
+    width: "32%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "4%",
+    borderRadius: 10,
+    elevation: 15, //Android
+    shadowColor: "#333333", //A partir de aqui ios
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  btnRegistro: {
+    backgroundColor: "#2272A7",
+    height: "8%",
+    width: "40%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: "24%",
+    borderRadius: 10,
+    elevation: 15, //Android
+    shadowColor: "#333333", //A partir de aqui ios
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  separator: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+    marginTop: "5%",
+    marginBottom: "5%",
+  },
+  line: {
+    height: 1,
+    width: 120,
+    backgroundColor: "black",
   },
 });
