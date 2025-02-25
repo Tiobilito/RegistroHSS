c1f59a2babe931cda65106831334fb6375a66bef
André Josué Marín González <andre.marin1135@alumnos.udg.mx>
2025-02-25 13:41:15 -0600

    CL

--- Scenes/Ayuda.js
+++ Scenes/Ayuda.js
@@ -11,7 +11,6 @@ import {
   ActivityIndicator,
 } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import ollama from "ollama";
 
 const handleGenericAPIRequest = async (message) => {
   console.log("El mensaje enviado es: " + message);
